be3dbb7be03a9fd508100b2f7f61f59f86760da0
Tatiana Snook <tatsnook@gmail.com>
2024-12-12 20:44:02 -0600

    studio: spa user signup

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,30 @@
+package org.launchcode.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @GetMapping()
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Error error, String verify) {
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        } else {
+            model.addAttribute("error", "Error. Please try again.");
+            return "user/add";
+        }
+    }
+}
