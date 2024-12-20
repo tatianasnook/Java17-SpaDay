a2fd95c2cd1b079dc0d41ce8c1c26842985b63a7
Tatiana Snook <tatsnook@gmail.com>
2024-12-19 20:10:32 -0600

    added validation

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -4,27 +4,31 @@ import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
+
     @GetMapping()
     public String displayAddUserForm(Model model) {
-        model.addAttribute(new User());
+        model.addAttribute("user", new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Error error, String verify) {
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
-        } else {
-            model.addAttribute("error", "Error. Please try again.");
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        if (errors.hasErrors()) {
             return "user/add";
         }
+        if (!user.getPassword().equals(verify)) {
+            model.addAttribute("error", "Password do not match.");
+            return "user/add";
+        }
+        return "user/index";
     }
 }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,16 +1,32 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required.")
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Must be a valid email address.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min=6, message="Password must be at least 6 characters.")
     private String password;
+
+    @NotBlank(message = "Password verification is required.")
+    private String verify;
 
     public User () {}
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -35,5 +51,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify (String verify) {
+        this.verify = verify;
     }
 }
